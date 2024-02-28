5a1d4f8538c425a75f4c15919e1576028b7bf20f
Lane Bye <viridescentspiritgaming@gmail.com>
2024-02-28 11:00:02 -0600

    Create and script add book form
    Created new book form element in html. Set display to hidden and added
    button that shows the form in the window. Added button that accepts
    the input values and uses them to add the book details to the library
    array. Button also adds the new book to the window as a div element so
    that it appears with the pre-existing books on the page.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const myLibrary = [];
 
+const showFormButton = document.getElementById('new_book');
+showFormButton.addEventListener('click', showForm.bind());
+
 function book(title, author, pages, read) {
         this.title = title;
         this.author = author;
@@ -11,7 +14,7 @@ function addBookToLibrary() {
     const bookAdd = new book(title, author, pages, read);
 
     myLibrary.push(bookAdd);
-}
+};
 
 function displayLibrary() {
     let libraryList = document.getElementById('library');
@@ -35,6 +38,46 @@ function displayLibrary() {
     });
 };
 
+function acceptBook(form) {
+    title = form.title.value;
+    author = form.author.value;
+    pages = form.pages.value;
+    read = form.read.value;
+
+    addBookToLibrary(title, author, pages, read);
+    addToDisplay();
+
+    form.title.value = '';
+    form.author.value = '';
+    form.pages.value = '';
+    document.getElementById('read_book').checked = false;
+    document.getElementById('unread_book').checked = false;
+    document.getElementById('new_book_form').style.display = 'none';
+};
+
+function addToDisplay() {
+    let libraryList = document.getElementById('library');
+    let i = myLibrary.length;
+    let book = myLibrary[i-1];
+
+    let libraryBook = document.createElement('div');
+    libraryBook.setAttribute('id','bookDiv'+i);
+    libraryList.appendChild(libraryBook);
+
+    for (const property in book) {
+        let currentBook = document.getElementById('bookDiv'+i);
+        let newPara = document.createElement('p');
+
+        newPara.textContent = book[property];
+
+        currentBook.appendChild(newPara);
+    };
+};
+
+function showForm() {
+    document.getElementById('new_book_form').style.display ='block';
+}
+
 title = 'The Hobbit';
 author = 'JRR Tolkien';
 pages = 1000;
@@ -48,6 +91,5 @@ pages = 500;
 read = false;
 
 addBookToLibrary();
-console.log(myLibrary);
 
 displayLibrary();
